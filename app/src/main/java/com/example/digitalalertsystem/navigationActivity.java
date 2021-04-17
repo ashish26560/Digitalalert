abfcb7ba6a33b367c99b9aee6743446524671144
Ashish Chauhan <65843706+ashish26560@users.noreply.github.com>
2021-04-17 13:18:04 +0530

    Added headerView, for accessing the header of navigation drawer and showing the current user name.

--- app/src/main/java/com/example/digitalalertsystem/navigationActivity.java
+++ app/src/main/java/com/example/digitalalertsystem/navigationActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -18,6 +20,12 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class navigationActivity extends AppCompatActivity {
@@ -26,8 +34,18 @@ public class navigationActivity extends AppCompatActivity {
     Toolbar toolbar;
     NavigationView navView;
     FirebaseAuth auth;
+    private FirebaseUser user;
+
+    private DatabaseReference reference;
+    private String userID;
     //    for creating the actionbar
     ActionBarDrawerToggle toggle;
+
+    String fullname;
+
+    View mHeaderView;
+
+    TextView Name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +55,51 @@ public class navigationActivity extends AppCompatActivity {
 //        Toolbar toolbar =(Toolbar)findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
+//
         auth = FirebaseAuth.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
 
         drawer = findViewById(R.id.drawer);
         toolbar = findViewById(R.id.toolbar);
         navView = findViewById(R.id.navView);
-        setSupportActionBar(toolbar);
 
+
+        // NavigationView Header
+        mHeaderView =  navView.getHeaderView(0);
+
+
+        // View
+        Name = (TextView) mHeaderView.findViewById(R.id.username);
+        setSupportActionBar(toolbar);
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userprofile = snapshot.getValue(User.class);
+
+                if (userprofile != null) {
+                    fullname = userprofile.fullname;
+//                    userPhoneNo = userprofile.userPhoneNo;
+//                    emergencyPhoneNo1 = userprofile.emergencyPhoneNo1;
+//                    emergencyPhoneNo2 = userprofile.emergencyPhoneNo2;
+
+                    Name.setText(fullname);
+//                    UserPhoneNo.setText(userPhoneNo);
+//                    EmergencyPhoneNo1.setText(emergencyPhoneNo1);
+//                    EmergencyPhoneNo2.setText(emergencyPhoneNo2);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(navigationActivity.this, "Something Wrong", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        // Set username & email
+//        Name.setText(SharedPrefManager.getInstance(this).getUsername());
 
 
         toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open, R.string.close);
